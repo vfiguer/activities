e1d43cc5830bf5da6ae0faa03c85dc0bdc1a5b79
vfiguer <136345153+vfiguer@users.noreply.github.com>
2024-11-08 17:14:57 +0100

    Add files via upload

--- Activity_4_2/script/script.js
+++ Activity_4_2/script/script.js
@@ -1,8 +1,37 @@
+flag = false
+
 function changeTextSize() {
     var input = document.getElementById('font-size').value;
-    
-    document.querySelectorAll(".txt").style.fontSize = input + "px";
+    document.querySelector(".form").style.fontSize = input + "em";
   }
 
+function changeSeparation() {
+  var input = document.getElementById('separation').value;
+  document.querySelector(".form").style.letterSpacing = input + "em";
+}
 
+function bold() {
+  if(flag==false){
+    document.querySelector(".form").style.fontWeight = "bolder" 
+    flag =true}
+    else{    
+    document.querySelector(".form").removeAttribute("style")
+    flag =false
+}
+}
   
+function grayScale(){
+
+}
+function darkContrast(){
+
+}
+function lightContrast(){
+
+}
+function highSaturation(){
+
+}
+function lowSaturation(){
+  
+}
