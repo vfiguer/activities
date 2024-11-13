0a9328223a883d0842b6886d834dc892af3835dd
vfiguer <136345153+vfiguer@users.noreply.github.com>
2024-11-13 22:59:34 +0100

    Add files via upload

--- Activity_4_2/script/script.js
+++ Activity_4_2/script/script.js
@@ -1,37 +1,47 @@
-flag = false
+//variables de inputs
+var accesibilityPopup = document.querySelector(".accesibilityPopup");
+var accesibilityBtn = document.getElementById("accesibilityBtn");
+var saveAccesibility = document.getElementById("saveAccesibility");
+var colorMode = document.getElementById("colorMode");
+var size = document.getElementById("size");
+var letterSpacing = document.getElementById("letterSpacing");
+var lineSpacing = document.getElementById("lineSpacing");
+var wordSpacing = document.getElementById("wordSpacing");
+//donde se aplican los cambios al css y clases
+var body = document.querySelector("body");
+var html = document.querySelector("html");
+//reinicio al formulario
+size.value=100;
+letterSpacing.value=0;
+lineSpacing.value=0;
+wordSpacing.value=0;
+colorMode.value="normal";
 
-function changeTextSize() {
-    var input = document.getElementById('font-size').value;
-    document.querySelector(".form").style.fontSize = input + "em";
-  }
+//eventos
+accesibilityPopup.style.display = "none";
+accesibilityBtn.addEventListener("click", () => {
+  accesibilityPopup.style.display = "flex";
+});
+saveAccesibility.addEventListener("click", () => {
+  accesibilityPopup.style.display = "none";
+});
 
-function changeSeparation() {
-  var input = document.getElementById('separation').value;
-  document.querySelector(".form").style.letterSpacing = input + "em";
-}
+colorMode.addEventListener("change", (e) => {
+  body.classList = e.target.value;
+});
 
-function bold() {
-  if(flag==false){
-    document.querySelector(".form").style.fontWeight = "bolder" 
-    flag =true}
-    else{    
-    document.querySelector(".form").removeAttribute("style")
-    flag =false
-}
-}
-  
-function grayScale(){
+size.addEventListener("change", (e) => {
+  html.style.fontSize = e.target.value+"%";
+});
 
-}
-function darkContrast(){
+letterSpacing.addEventListener("change", (e)=>{
+  html.style.letterSpacing = e.target.value + "px";
+})
 
-}
-function lightContrast(){
+wordSpacing.addEventListener("change", (e)=>{
+  html.style.wordSpacing = e.target.value + "px";
+})
 
-}
-function highSaturation(){
-
-}
-function lowSaturation(){
-  
-}
+lineSpacing.addEventListener("change", (e)=>{
+  html.style.lineSpacing = e.target.value + "px";
+})
